9f143148c70f2b686fbcf2ffeaf978b798a24076
Guigzouz <benard.guillaume0@gmail.com>
2024-06-12 11:59:56 +0200

    result to el

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ console.log("hello worleziejid");
 const handleClick = () => {
   console.log("clicked");
   var search = document.getElementById("search").value;
-  var results = document.getElementById("results");
-  results.innerHTML = search;
+  var el = document.getElementById("results");
+  el.innerHTML = search;
 };
